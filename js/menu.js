8fe74c3a9c56842555a53f70fac1b9eb0635a7f7
shandykate <shandy.kate@gmail.com>
2023-01-11 22:25:19 +0200

    Removes class .active for header-menu

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,7 @@ $('.header-burger').click(function(event){
 $('.header-burger, .header-menu').toggleClass('active');
 $('body').toggleClass('lock');
 });
+$('.header-link').click(function(event){
+    $('.header-burger, .header-menu').removeClass('active');
+});
 });
