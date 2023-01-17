7dd4d8b6eb47d972f5b9608ddee819e0261370b5
shandykate <shandy.kate@gmail.com>
2023-01-17 09:05:44 +0200

    Adds gulp, adss support

---
+++ gulp/config/path.js
@@ -0,0 +1,25 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+export const path = {
+    build: {
+        html: `${srcFolder}/`,
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {
+        html: `${srcFolder}/**/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ``,
+}
